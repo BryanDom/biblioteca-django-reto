730e152a554d2afb06496046c59fc82372bf7bbb
brayandom1604 <brayandom1604@gmail.com>
2026-05-12 17:20:52 -0600

    Descripción: Se agrega y se registran los modelos en el admin con filtros, búsqueda, crear,editar,etc.

--- biblioteca/admin.py
+++ biblioteca/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
 
-# Register your models here.
+from .models import Autor, Libro, Usuario, Prestamo
+
+
+@admin.register(Autor)
+class AutorAdmin(ImportExportModelAdmin):
+    # Columnas que estan en el listado
+    list_display = ("nombre", "apellido", "nacionalidad")
+    # Se buscan por el nombre, apellido o nacionalidad
+    search_fields = ("nombre", "apellido", "nacionalidad")
+    list_filter = ("nacionalidad",)
+
+
+@admin.register(Libro)
+class LibroAdmin(ImportExportModelAdmin):
+    list_display = ("titulo", "nombre_autor", "numero_isbn", "cantidad_libros_disponibles")
+    search_fields = ("titulo", "numero_isbn", "nombre_autor__nombre", "nombre_autor__apellido")
+    list_filter = ("nombre_autor",)
+    # Aqui se puede editar la cantidad de libros disponibles
+    list_editable = ("cantidad_libros_disponibles",)
+
+
+@admin.register(Usuario)
+class UsuarioAdmin(ImportExportModelAdmin):
+    list_display = ("nombre", "apellido_paterno", "apellido_materno", "correo_electronico", "numero_telefono")
+    search_fields = ("nombre", "apellido_paterno", "apellido_materno", "correo_electronico")
+    list_filter = ("apellido_paterno",)
+
+
+@admin.register(Prestamo)
+class PrestamoAdmin(ImportExportModelAdmin):
+    list_display = (
+        "nombre_usuario",
+        "nombre_libro",
+        "fecha_inicio_prestamo",
+        "fecha_devolucion_esperada",
+        "prestamo_activo",
+    )
+    search_fields = (
+        "nombre_usuario__nombre",
+        "nombre_usuario__apellido_paterno",
+        "nombre_libro__titulo",
+    )
+    list_filter = ("prestamo_activo", "fecha_inicio_prestamo")
+    list_editable = ("prestamo_activo",)
+    readonly_fields = ("fecha_inicio_prestamo",)
